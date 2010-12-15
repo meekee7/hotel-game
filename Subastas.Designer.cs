be3a4395cd53a6789313bb0e14c4699704130bef
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2010-12-15 15:52:02 +0000

    Avanzando mucho con las subastas, falta terminar las comprobaciones y hacer la venta
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@13 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Subastas.Designer.cs
+++ Subastas.Designer.cs
@@ -30,18 +30,32 @@
         {
             this.bCerrar = new System.Windows.Forms.Button();
             this.etiHotel = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listaHoteles = new System.Windows.Forms.ComboBox();
             this.bHotel = new System.Windows.Forms.Button();
             this.grupoHotel = new System.Windows.Forms.GroupBox();
+            this.bVerHoteles = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.Label();
+            this.bJ1 = new System.Windows.Forms.Button();
+            this.bJ3 = new System.Windows.Forms.Button();
+            this.bJ2 = new System.Windows.Forms.Button();
+            this.bJ4 = new System.Windows.Forms.Button();
+            this.cantidad = new System.Windows.Forms.TextBox();
+            this.txt_precio_mayor = new System.Windows.Forms.Label();
+            this.precio_mayor = new System.Windows.Forms.Label();
+            this.grupoEstado = new System.Windows.Forms.GroupBox();
+            this.txt_mayor_postor = new System.Windows.Forms.Label();
+            this.mayor_postor = new System.Windows.Forms.Label();
+            this.bVender = new System.Windows.Forms.Button();
             this.grupoHotel.SuspendLayout();
+            this.grupoEstado.SuspendLayout();
             this.SuspendLayout();
             // 
             // bCerrar
             // 
             this.bCerrar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.bCerrar.Location = new System.Drawing.Point(486, 273);
+            this.bCerrar.Location = new System.Drawing.Point(34, 188);
             this.bCerrar.Name = "bCerrar";
-            this.bCerrar.Size = new System.Drawing.Size(75, 23);
+            this.bCerrar.Size = new System.Drawing.Size(79, 23);
             this.bCerrar.TabIndex = 0;
             this.bCerrar.Text = "Cerrar";
             this.bCerrar.UseVisualStyleBackColor = true;
@@ -55,34 +69,172 @@
             this.etiHotel.TabIndex = 1;
             this.etiHotel.Text = "Selecciona Hotel:";
             // 
-            // comboBox1
+            // listaHoteles
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(107, 39);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.listaHoteles.FormattingEnabled = true;
+            this.listaHoteles.Location = new System.Drawing.Point(107, 39);
+            this.listaHoteles.Name = "listaHoteles";
+            this.listaHoteles.Size = new System.Drawing.Size(121, 21);
+            this.listaHoteles.TabIndex = 2;
+            this.listaHoteles.SelectedIndexChanged += new System.EventHandler(this.listaHoteles_SelectedIndexChanged);
             // 
             // bHotel
             // 
-            this.bHotel.Location = new System.Drawing.Point(62, 71);
+            this.bHotel.Enabled = false;
+            this.bHotel.Location = new System.Drawing.Point(107, 71);
             this.bHotel.Name = "bHotel";
             this.bHotel.Size = new System.Drawing.Size(75, 23);
             this.bHotel.TabIndex = 3;
             this.bHotel.Text = "Subastar";
             this.bHotel.UseVisualStyleBackColor = true;
+            this.bHotel.Click += new System.EventHandler(this.bHotel_Click);
             // 
             // grupoHotel
             // 
             this.grupoHotel.Controls.Add(this.bHotel);
             this.grupoHotel.Controls.Add(this.etiHotel);
-            this.grupoHotel.Controls.Add(this.comboBox1);
-            this.grupoHotel.Location = new System.Drawing.Point(50, 104);
+            this.grupoHotel.Controls.Add(this.listaHoteles);
+            this.grupoHotel.Location = new System.Drawing.Point(21, 22);
             this.grupoHotel.Name = "grupoHotel";
-            this.grupoHotel.Size = new System.Drawing.Size(234, 100);
+            this.grupoHotel.Size = new System.Drawing.Size(248, 100);
             this.grupoHotel.TabIndex = 4;
             this.grupoHotel.TabStop = false;
             this.grupoHotel.Text = "Subastar Hotel";
+            // 
+            // bVerHoteles
+            // 
+            this.bVerHoteles.Location = new System.Drawing.Point(170, 188);
+            this.bVerHoteles.Name = "bVerHoteles";
+            this.bVerHoteles.Size = new System.Drawing.Size(79, 23);
+            this.bVerHoteles.TabIndex = 4;
+            this.bVerHoteles.Text = "Ver Hoteles";
+            this.bVerHoteles.UseVisualStyleBackColor = true;
+            this.bVerHoteles.Click += new System.EventHandler(this.bVerHoteles_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.AutoSize = true;
+            this.txtCantidad.Location = new System.Drawing.Point(21, 30);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(97, 13);
+            this.txtCantidad.TabIndex = 5;
+            this.txtCantidad.Text = "Cantidad a ofrecer:";
+            // 
+            // bJ1
+            // 
+            this.bJ1.Enabled = false;
+            this.bJ1.Location = new System.Drawing.Point(43, 54);
+            this.bJ1.Name = "bJ1";
+            this.bJ1.Size = new System.Drawing.Size(75, 23);
+            this.bJ1.TabIndex = 6;
+            this.bJ1.Text = "Pujar J1";
+            this.bJ1.UseVisualStyleBackColor = true;
+            this.bJ1.Click += new System.EventHandler(this.bJ1_Click);
+            // 
+            // bJ3
+            // 
+            this.bJ3.Enabled = false;
+            this.bJ3.Location = new System.Drawing.Point(43, 83);
+            this.bJ3.Name = "bJ3";
+            this.bJ3.Size = new System.Drawing.Size(75, 23);
+            this.bJ3.TabIndex = 7;
+            this.bJ3.Text = "Pujar J3";
+            this.bJ3.UseVisualStyleBackColor = true;
+            this.bJ3.Click += new System.EventHandler(this.bJ3_Click);
+            // 
+            // bJ2
+            // 
+            this.bJ2.Enabled = false;
+            this.bJ2.Location = new System.Drawing.Point(124, 54);
+            this.bJ2.Name = "bJ2";
+            this.bJ2.Size = new System.Drawing.Size(75, 23);
+            this.bJ2.TabIndex = 8;
+            this.bJ2.Text = "Pujar J2";
+            this.bJ2.UseVisualStyleBackColor = true;
+            this.bJ2.Click += new System.EventHandler(this.bJ2_Click);
+            // 
+            // bJ4
+            // 
+            this.bJ4.Enabled = false;
+            this.bJ4.Location = new System.Drawing.Point(124, 83);
+            this.bJ4.Name = "bJ4";
+            this.bJ4.Size = new System.Drawing.Size(75, 23);
+            this.bJ4.TabIndex = 9;
+            this.bJ4.Text = "Pujar J4";
+            this.bJ4.UseVisualStyleBackColor = true;
+            this.bJ4.Click += new System.EventHandler(this.bJ4_Click);
+            // 
+            // cantidad
+            // 
+            this.cantidad.Enabled = false;
+            this.cantidad.Location = new System.Drawing.Point(124, 28);
+            this.cantidad.Name = "cantidad";
+            this.cantidad.Size = new System.Drawing.Size(75, 20);
+            this.cantidad.TabIndex = 10;
+            // 
+            // txt_precio_mayor
+            // 
+            this.txt_precio_mayor.AutoSize = true;
+            this.txt_precio_mayor.Location = new System.Drawing.Point(31, 117);
+            this.txt_precio_mayor.Name = "txt_precio_mayor";
+            this.txt_precio_mayor.Size = new System.Drawing.Size(87, 13);
+            this.txt_precio_mayor.TabIndex = 11;
+            this.txt_precio_mayor.Text = "Máximo ofrecido:";
+            // 
+            // precio_mayor
+            // 
+            this.precio_mayor.AutoSize = true;
+            this.precio_mayor.Location = new System.Drawing.Point(121, 117);
+            this.precio_mayor.Name = "precio_mayor";
+            this.precio_mayor.Size = new System.Drawing.Size(0, 13);
+            this.precio_mayor.TabIndex = 12;
+            // 
+            // grupoEstado
+            // 
+            this.grupoEstado.Controls.Add(this.bVender);
+            this.grupoEstado.Controls.Add(this.mayor_postor);
+            this.grupoEstado.Controls.Add(this.txt_mayor_postor);
+            this.grupoEstado.Controls.Add(this.txtCantidad);
+            this.grupoEstado.Controls.Add(this.precio_mayor);
+            this.grupoEstado.Controls.Add(this.bJ1);
+            this.grupoEstado.Controls.Add(this.txt_precio_mayor);
+            this.grupoEstado.Controls.Add(this.bJ3);
+            this.grupoEstado.Controls.Add(this.cantidad);
+            this.grupoEstado.Controls.Add(this.bJ2);
+            this.grupoEstado.Controls.Add(this.bJ4);
+            this.grupoEstado.Location = new System.Drawing.Point(285, 22);
+            this.grupoEstado.Name = "grupoEstado";
+            this.grupoEstado.Size = new System.Drawing.Size(226, 201);
+            this.grupoEstado.TabIndex = 13;
+            this.grupoEstado.TabStop = false;
+            this.grupoEstado.Text = "Estado de la subasta";
+            // 
+            // txt_mayor_postor
+            // 
+            this.txt_mayor_postor.AutoSize = true;
+            this.txt_mayor_postor.Location = new System.Drawing.Point(47, 141);
+            this.txt_mayor_postor.Name = "txt_mayor_postor";
+            this.txt_mayor_postor.Size = new System.Drawing.Size(71, 13);
+            this.txt_mayor_postor.TabIndex = 14;
+            this.txt_mayor_postor.Text = "Mayor postor:";
+            // 
+            // mayor_postor
+            // 
+            this.mayor_postor.AutoSize = true;
+            this.mayor_postor.Location = new System.Drawing.Point(121, 141);
+            this.mayor_postor.Name = "mayor_postor";
+            this.mayor_postor.Size = new System.Drawing.Size(0, 13);
+            this.mayor_postor.TabIndex = 15;
+            // 
+            // bVender
+            // 
+            this.bVender.Location = new System.Drawing.Point(79, 166);
+            this.bVender.Name = "bVender";
+            this.bVender.Size = new System.Drawing.Size(75, 23);
+            this.bVender.TabIndex = 14;
+            this.bVender.Text = "Vender";
+            this.bVender.UseVisualStyleBackColor = true;
+            this.bVender.Click += new System.EventHandler(this.bVender_Click);
             // 
             // Subastas
             // 
@@ -90,8 +242,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.bCerrar;
-            this.ClientSize = new System.Drawing.Size(573, 308);
+            this.ClientSize = new System.Drawing.Size(530, 240);
             this.ControlBox = false;
+            this.Controls.Add(this.grupoEstado);
+            this.Controls.Add(this.bVerHoteles);
             this.Controls.Add(this.grupoHotel);
             this.Controls.Add(this.bCerrar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -99,6 +253,8 @@
             this.Text = "Subastas";
             this.grupoHotel.ResumeLayout(false);
             this.grupoHotel.PerformLayout();
+            this.grupoEstado.ResumeLayout(false);
+            this.grupoEstado.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,8 +263,21 @@
 
         private System.Windows.Forms.Button bCerrar;
         private System.Windows.Forms.Label etiHotel;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox listaHoteles;
         private System.Windows.Forms.Button bHotel;
         private System.Windows.Forms.GroupBox grupoHotel;
+        private System.Windows.Forms.Button bVerHoteles;
+        private System.Windows.Forms.Label txtCantidad;
+        private System.Windows.Forms.Button bJ1;
+        private System.Windows.Forms.Button bJ3;
+        private System.Windows.Forms.Button bJ2;
+        private System.Windows.Forms.Button bJ4;
+        private System.Windows.Forms.TextBox cantidad;
+        private System.Windows.Forms.Label txt_precio_mayor;
+        private System.Windows.Forms.Label precio_mayor;
+        private System.Windows.Forms.GroupBox grupoEstado;
+        private System.Windows.Forms.Label mayor_postor;
+        private System.Windows.Forms.Label txt_mayor_postor;
+        private System.Windows.Forms.Button bVender;
     }
 }
--- Subastas.cs
+++ Subastas.cs
@@ -16,6 +16,9 @@ namespace Juego_Hotel
         public int n_500;
         public int n_100;
         public int n_50;
+        Juego juego;
+        int n_mayor_postor; // nº de jugador
+        int n_precio_mayor = 0;
 
         public Subastas(ref Juego juego)
         {
@@ -25,6 +28,153 @@ namespace Juego_Hotel
             this.n_500 = 0;
             this.n_100 = 0;
             this.n_50 = 0;
+            this.juego = juego;
+            LinkedList<Hotel> lista = this.juego.jugador_actual.hoteles;
+            this.listaHoteles.BeginUpdate();
+            this.listaHoteles.Items.Clear();
+            foreach (Hotel hotel in lista)
+                this.listaHoteles.Items.Add(hotel.nombre_txt);
+            this.listaHoteles.EndUpdate();
+        }
+
+        private void listaHoteles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.bHotel.Enabled = true;
+        }
+
+        private void bHotel_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("¿Estás seguro de que quieres subastar el hotel?\nEsta operación no se puede cancelar",
+                                               "Confirmación de inicio de subasta", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                cantidad.Enabled = true;
+                bJ1.Enabled = true;
+                bJ2.Enabled = true;
+                if (this.juego.n_jugadores > 2)
+                    bJ3.Enabled = true;
+                if (this.juego.n_jugadores > 3)
+                    bJ4.Enabled = true;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VerHoteles frm_ver_hoteles = new VerHoteles(ref this.juego, 0, true);
+            frm_ver_hoteles.ShowDialog();
+        }
+
+        private void bVerHoteles_Click(object sender, EventArgs e)
+        {
+            VerHoteles frm_ver_hoteles = new VerHoteles(ref this.juego, 0, true);
+            frm_ver_hoteles.ShowDialog();
+        }
+
+        private void bVender_Click(object sender, EventArgs e)
+        {
+            if (this.precio_mayor.Text.ToString() == "")
+                MessageBox.Show("No se ha pujado todavía");
+        }
+
+        private void bJ1_Click(object sender, EventArgs e)
+        {
+            String cantidad = this.cantidad.Text.ToString().Trim();
+            int cantidad_num;
+            if (cantidad == "" || cantidad == "0")
+                MessageBox.Show("No se ha introducido una cantidad válida");
+            else if (int.TryParse(cantidad, out cantidad_num) == false)
+                MessageBox.Show("No se ha introducido un número entero");
+            else if (cantidad_num < 0)
+                MessageBox.Show("No se pueden introducir cantidades negativas");
+            else if (cantidad_num <= n_precio_mayor)
+                MessageBox.Show("La cantidad es menor o igual que la puja máxima");
+            else
+            {
+                if (MessageBox.Show("¿Estás seguro de que quieres realizar la puja?",
+                                    "Confirmación de puja", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    this.n_precio_mayor = cantidad_num;
+                    this.precio_mayor.Text = cantidad_num.ToString();
+                    this.mayor_postor.Text = "J1 - " + this.juego.jugadores[0].color.ToString();
+                    this.n_mayor_postor = 0;
+                }
+            }
+        }
+
+        private void bJ2_Click(object sender, EventArgs e)
+        {
+            String cantidad = this.cantidad.Text.ToString().Trim();
+            int cantidad_num;
+            if (cantidad == "" || cantidad == "0")
+                MessageBox.Show("No se ha introducido una cantidad válida");
+            else if (int.TryParse(cantidad, out cantidad_num) == false)
+                MessageBox.Show("No se ha introducido un número entero");
+            else if (cantidad_num < 0)
+                MessageBox.Show("No se pueden introducir cantidades negativas");
+            else if (cantidad_num <= n_precio_mayor)
+                MessageBox.Show("La cantidad es menor o igual que la puja máxima");
+            else
+            {
+                if (MessageBox.Show("¿Estás seguro de que quieres realizar la puja?",
+                                    "Confirmación de puja", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    this.n_precio_mayor = cantidad_num;
+                    this.precio_mayor.Text = cantidad_num.ToString();
+                    this.mayor_postor.Text = "J2 - " + this.juego.jugadores[1].color.ToString();
+                    this.n_mayor_postor = 0;
+                }
+            }
+        }
+
+        private void bJ3_Click(object sender, EventArgs e)
+        {
+            String cantidad = this.cantidad.Text.ToString().Trim();
+            int cantidad_num;
+            if (cantidad == "" || cantidad == "0")
+                MessageBox.Show("No se ha introducido una cantidad válida");
+            else if (int.TryParse(cantidad, out cantidad_num) == false)
+                MessageBox.Show("No se ha introducido un número entero");
+            else if (cantidad_num < 0)
+                MessageBox.Show("No se pueden introducir cantidades negativas");
+            else if (cantidad_num <= n_precio_mayor)
+                MessageBox.Show("La cantidad es menor o igual que la puja máxima");
+            else
+            {
+                if (MessageBox.Show("¿Estás seguro de que quieres realizar la puja?",
+                                    "Confirmación de puja", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    this.n_precio_mayor = cantidad_num;
+                    this.precio_mayor.Text = cantidad_num.ToString();
+                    this.mayor_postor.Text = "J3 - " + this.juego.jugadores[2].color.ToString();
+                    this.n_mayor_postor = 2;
+                }
+            }
+        }
+
+        private void bJ4_Click(object sender, EventArgs e)
+        {
+            String cantidad = this.cantidad.Text.ToString().Trim();
+            int cantidad_num;
+            if (cantidad == "" || cantidad == "0")
+                MessageBox.Show("No se ha introducido una cantidad válida");
+            else if (int.TryParse(cantidad, out cantidad_num) == false)
+                MessageBox.Show("No se ha introducido un número entero");
+            else if (cantidad_num < 0)
+                MessageBox.Show("No se pueden introducir cantidades negativas");
+            else if (cantidad_num <= n_precio_mayor)
+                MessageBox.Show("La cantidad es menor o igual que la puja máxima");
+            else
+            {
+                if (MessageBox.Show("¿Estás seguro de que quieres realizar la puja?",
+                                    "Confirmación de puja", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    this.n_precio_mayor = cantidad_num;
+                    this.precio_mayor.Text = cantidad_num.ToString();
+                    this.mayor_postor.Text = "J4 - " + this.juego.jugadores[3].color.ToString();
+                    this.n_mayor_postor = 3;
+                }
+            }
         }
     }
 }
