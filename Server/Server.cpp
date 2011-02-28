9f6dd392d06d62dfd20cd9351a099ea7872ea365
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2011-02-28 16:56:01 +0000

    Señales
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@50 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 #include <stdio.h>
 #include "portable_socket.h"
+#include <cstdlib>
+#include <signal.h>
+#include <windows.h>
 
 using namespace std;
+
+volatile int cerrando = 0;
+
+void cerrar (int signum)
+{
+	cerrando = 1;
+}
+
+void capturar_señales()
+{
+	signal(SIGINT, cerrar);
+    signal(SIGTERM, cerrar);
+    #ifdef _WIN32
+		signal(SIGBREAK, cerrar);
+    #endif
+}
+
+void quitar_captura_señales()
+{
+    signal(SIGINT, 0);
+    signal(SIGTERM, 0);
+    #ifdef _WIN32
+		signal(SIGBREAK, 0);
+    #endif
+}
 
 void hacer_de_server()
 {
@@ -11,17 +39,33 @@ void hacer_de_server()
    server_info.sin_family=AF_INET;
    server_info.sin_port=htons(12345);
    server_info.sin_addr.s_addr=INADDR_ANY;
-   socket->pbind((sockaddr*) &server_info,sizeof(server_info));
-   socket->plisten(SOMAXCONN);
-   cout << "Hecho, error: " << socket->get_last_error() << endl;
+   if (socket->pbind((sockaddr*) &server_info,sizeof(server_info)) < 0)
+   {
+	   cout << "Error en bind: " << socket->get_last_error() << endl;
+	   delete socket;
+	   return;
+   }
+   if (socket->plisten(SOMAXCONN) < 0)
+   {
+	   cout << "Error en listen: " << socket->get_last_error() << endl;
+	   delete socket;
+	   return;
+   }
    sockaddr_in client_info;
    socklen_t addrlen = sizeof(client_info);
+   capturar_señales();
+   while (cerrando == 0)
+   {
+   }
+   printf ("Cerrando\n");
+   quitar_captura_señales();
    portable_socket* socket_cliente = socket->paccept((sockaddr*) &client_info, &addrlen);
    printf("Conexión de cliente desde %s:%d\n", inet_ntoa(client_info.sin_addr), ntohs(client_info.sin_port));
-   char* data = (char*) malloc(sizeof(char)*200);
+   /*char* data = (char*) malloc(sizeof(char)*200);
    int bytes = recv(socket_cliente->get_fd(),data, 200, 0);
    printf("Recibidos %i bytes: %s|\n", bytes, data);
-   printf("Enviados %i bytes\n", send(socket_cliente->get_fd(), "Adios", 6, 0));
+   printf("Enviados %i bytes\n", send(socket_cliente->get_fd(), "Adios", 6, 0));*/
+   system("pause");
    delete socket;
 }
 
@@ -50,5 +94,5 @@ void hacer_de_cliente()
 int main(int argc, char* argv[])
 {
    cout << "Hola" << endl;
-   hacer_de_cliente();
+   hacer_de_server();
 }
