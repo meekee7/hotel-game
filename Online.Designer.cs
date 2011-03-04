f97d49d3a19f80fca3b3975cbc61af7a7dd2640d
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2011-03-04 16:02:23 +0000

    Control datos en Online
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@68 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Online.Designer.cs
+++ Online.Designer.cs
@@ -28,28 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
             this.bLogin = new System.Windows.Forms.Button();
             this.bConectar = new System.Windows.Forms.Button();
+            this.txtServidor = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtLogin
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtLogin.Location = new System.Drawing.Point(93, 44);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 0;
             // 
             // bLogin
             // 
             this.bLogin.Enabled = false;
-            this.bLogin.Location = new System.Drawing.Point(199, 13);
+            this.bLogin.Location = new System.Drawing.Point(12, 42);
             this.bLogin.Name = "bLogin";
             this.bLogin.Size = new System.Drawing.Size(75, 23);
             this.bLogin.TabIndex = 1;
             this.bLogin.Text = "Login";
             this.bLogin.UseVisualStyleBackColor = true;
-            this.bLogin.Click += new System.EventHandler(this.button1_Click);
+            this.bLogin.Click += new System.EventHandler(this.bLogin_Click);
             // 
             // bConectar
             // 
@@ -59,16 +60,24 @@
             this.bConectar.TabIndex = 2;
             this.bConectar.Text = "Conectar";
             this.bConectar.UseVisualStyleBackColor = true;
-            this.bConectar.Click += new System.EventHandler(this.button2_Click);
+            this.bConectar.Click += new System.EventHandler(this.bConectar_Click);
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(93, 15);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 3;
             // 
             // Online
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 431);
+            this.Controls.Add(this.txtServidor);
             this.Controls.Add(this.bConectar);
             this.Controls.Add(this.bLogin);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtLogin);
             this.Name = "Online";
             this.Text = "Online";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Online_FormClosing);
@@ -79,8 +88,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtLogin;
         private System.Windows.Forms.Button bLogin;
         private System.Windows.Forms.Button bConectar;
+        private System.Windows.Forms.TextBox txtServidor;
     }
 }
--- Online.cs
+++ Online.cs
@@ -21,27 +21,45 @@ namespace Juego_Hotel
             this.interfaz = interfaz;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void bLogin_Click(object sender, EventArgs e)
         {
             if (socket.Connected)
             {
-                socket.Send(Encoding.UTF8.GetBytes(this.textBox1.Text.ToCharArray()));
+                if (this.txtLogin.Text.Length == 0)
+                    MessageBox.Show("El apodo no puede estar vacío");
+                else if (this.txtLogin.Text.Length > 20)
+                    MessageBox.Show("El apodo no puede superar 20 caracteres");
+                else
+                {
+                    socket.Send(Encoding.UTF8.GetBytes(this.txtLogin.Text.ToCharArray()));
+                    byte[] data = new byte[20];
+                    int bytes_recibidos = socket.Receive(data);
+                    if (System.Text.Encoding.UTF8.GetString(data, 0, bytes_recibidos) == "username in use")
+                    {
+                        MessageBox.Show("El apodo ya está en uso");
+                    }
+                    data = null;
+                }
             }
             else
                 MessageBox.Show("No estás conectado");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void bConectar_Click(object sender, EventArgs e)
         {
             try
             {
-                IPAddress dir = Dns.GetHostAddresses("localhost").First(IPAddress => IPAddress.AddressFamily == AddressFamily.InterNetwork);
+                IPAddress dir = Dns.GetHostAddresses(this.txtServidor.Text).First(IPAddress => IPAddress.AddressFamily == AddressFamily.InterNetwork);
                 IPEndPoint Ep = new IPEndPoint(dir, 12345);
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 socket.Connect(Ep);
                 this.bLogin.Enabled = true;
             }
             catch (SocketException ex)
+            {
+                MessageBox.Show("Error conectando: " + ex.Message);
+            }
+            catch (ArgumentException ex)
             {
                 MessageBox.Show("Error conectando: " + ex.Message);
             }
