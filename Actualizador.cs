cb014f30c7a94a364141fa035a33392f54ca4daa
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2011-11-28 03:13:29 +0000

    Añado módulo que comprueba si tienes la última versión
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@140 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

---
+++ Actualizador.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace Juego_Hotel
+{
+    class Actualizador
+    {
+        public String version_actual;
+        
+        public Actualizador()
+        {
+            this.version_actual = "1.03";
+        }
+        public Boolean comprobar_actualizacion()
+        {
+            System.Net.HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create("http://betovserver.no-ip.org/version_hotel.txt");
+
+            String resultado;
+
+            using (HttpWebResponse respuesta = (HttpWebResponse)peticion.GetResponse())
+            {
+                System.IO.StreamReader reader = new System.IO.StreamReader(respuesta.GetResponseStream());
+
+                resultado = reader.ReadToEnd();
+            }
+            resultado = resultado.Replace("\n", "").Replace("\r", "");
+            if (resultado == this.version_actual)
+                return false;
+            else
+                return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,10 +16,25 @@ namespace Juego_Hotel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            if (MessageBox.Show("¿Quieres jugar online?", "¡Bienvenido a Hotel!", MessageBoxButtons.YesNo) == DialogResult.No)
+            if (MessageBox.Show("¿Quieres comprobar si existe una versión del juego más actualizada?", "¡Bienvenido a Hotel!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Actualizador actualizador = new Actualizador();
+                if (actualizador.comprobar_actualizacion())
+                {
+                    if (MessageBox.Show("Existe una versión más nueva que la que estás usando (" + actualizador.version_actual + "). ¿Quieres acceder a la página del proyecto en SourceForge?\nEl juego se cerrará.",
+                        "¡Bienvenido a Hotel!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start("https://sourceforge.net/projects/hotels-game/files");
+                        return;
+                    }
+                }
+                else
+                    MessageBox.Show("No existe una versión más nueva :)");
+            }
+            //if (MessageBox.Show("¿Quieres jugar online?", "¡Bienvenido a Hotel!", MessageBoxButtons.YesNo) == DialogResult.No)
                 Application.Run(new Principal(false, null));
-            else
-                Application.Run(new Online());
+            //else
+                //Application.Run(new Online());
         }
     }
 }
