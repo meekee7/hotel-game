f2196ea93d53126b91abaec2e4c6bb9692e481ee
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2011-11-29 01:10:21 +0000

    Empezando el módulo que salva y carga partidas
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@143 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,9 @@ namespace Juego_Hotel
 
         static void Main ()
         {
+            Salvar_y_cargar mgr = new Salvar_y_cargar(null);
+            mgr.Salvar_partida("");
+            return;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             if (MessageBox.Show("¿Quieres comprobar si existe una versión del juego más actualizada?", "¡Bienvenido a Hotel!", MessageBoxButtons.YesNo) == DialogResult.Yes)
---
+++ Salvar_y_cargar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Juego_Hotel
+{
+    class Salvar_y_cargar
+    {
+        public Juego juego;
+
+        public Salvar_y_cargar(Juego juego)
+        {
+            this.juego = juego;
+        }
+
+        public Boolean Salvar_partida(String ruta)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+            
+            return true;
+        }
+
+        public Boolean Cargar_partida(String ruta)
+        {
+            return true;
+        }
+    }
+}
