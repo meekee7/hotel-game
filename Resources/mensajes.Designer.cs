d913cb2aa7d0fdfa2f93f5302c10db72ce7c5151
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2012-02-07 21:36:47 +0000

    Rename folder part 2
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@237 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Resources/mensajes.Designer.cs
+++ Resources/mensajes.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Juego_Hotel.resources {
+namespace Juego_Hotel.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Juego_Hotel.resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Juego_Hotel.resources.mensajes", typeof(mensajes).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Juego_Hotel.Resources.mensajes", typeof(mensajes).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
