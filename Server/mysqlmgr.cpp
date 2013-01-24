3d27a5e294cefdadb1eb40f6feae9721b8dc5814
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2013-01-24 11:59:35 +0000

    Fix compile under Windows (needs 32 bit connector) Needs testing in Linux
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@333 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Server/mysqlmgr.cpp
+++ Server/mysqlmgr.cpp
@@ -1,8 +1,22 @@
 #include "mysqlmgr.h"
 
+MySQLConnection* MySQLMgr::Connect (string host, string username, string password, string database)
+{
+	#ifdef _WIN32
+		this->conn->conn = this->driver->connect(host, username, password);
+		return this->conn;
+	#else
+		this->conn->conn = mysql_init(NULL);
+		mysql_real_connect(conn, host.c_str(), username.c_str(), password.c_str(), database.c_str(), 0, NULL, 0);
+	#endif
+}
 
 MySQLMgr::MySQLMgr(void)
 {
+	#ifdef _WIN32
+		this->driver = sql::mysql::get_mysql_driver_instance();
+	#endif
+	this->conn = new MySQLConnection();
 }
 
 MySQLMgr::~MySQLMgr(void)
--- Server/mysqlmgr.h
+++ Server/mysqlmgr.h
@@ -2,12 +2,24 @@
 #include <string>
 
 #ifdef _WIN32
-	#include "mysql_driver.h"
+	#include <mysql_driver.h>
+	#include <mysql_connection.h>
+	#include <cppconn\resultset.h>
 #else
 	#include "mysql/mysql.h"
 #endif
 
 using namespace std;
+
+class MySQLResult
+{
+public:
+	#ifdef _WIN32
+		sql::ResultSet* result;
+	#else
+		MYSQL_RES* result;
+	#endif
+};
 
 class MySQLConnection
 {
@@ -22,7 +34,11 @@ public:
 class MySQLMgr
 {
 public:
-	MySQLConnection Connect (string host, string username, string password, string database);
+	#ifdef _WIN32
+		sql::mysql::MySQL_Driver *driver;
+	#endif
+	MySQLConnection* conn;
+	MySQLConnection* Connect (string host, string username, string password, string database);
 	MySQLMgr(void);
 	~MySQLMgr(void);
 };
