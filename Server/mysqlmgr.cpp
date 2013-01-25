08aea3451a8009e6039c0b27d00835dba35f77aa
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2013-01-25 10:23:00 +0000

    Improve post-build events Implement data get methods. Needs testing and linux may not compile
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@345 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Server/mysqlmgr.cpp
+++ Server/mysqlmgr.cpp
@@ -3,34 +3,34 @@
 // Class MySQLMgr
 MySQLConnection* MySQLMgr::Connect (string host, string username, string password, string database)
 {
-	#ifdef _WIN32
-		try
-		{
-			this->connection->conn = this->driver->connect(host, username, password);
-		}
-		catch (sql::SQLException e)
-		{
-			wcout << "Error connecting to database: " << e.getSQLStateCStr() << " " << e.getErrorCode() << endl;
-		}
+    #ifdef _WIN32
+        try
+        {
+            this->connection->conn = this->driver->connect(host, username, password);
+        }
+        catch (sql::SQLException e)
+        {
+            wcout << "Error connecting to database: " << e.getSQLStateCStr() << " " << e.getErrorCode() << endl;
+        }
         this->connection->conn->setSchema(database);
-		return this->connection;
-	#else
-		this->connection->conn = mysql_init(NULL);
-		mysql_real_connect(this->connection->conn, host.c_str(), username.c_str(), password.c_str(), database.c_str(), 0, NULL, 0);
-      if (this->connection == NULL)
-         wcout << "Error connecting to database" << endl;
-      return this->connection;
-	#endif
+        return this->connection;
+    #else
+        this->connection->conn = mysql_init(NULL);
+        mysql_real_connect(this->connection->conn, host.c_str(), username.c_str(), password.c_str(), database.c_str(), 0, NULL, 0);
+        if (this->connection == NULL)
+            wcout << "Error connecting to database" << endl;
+        return this->connection;
+    #endif
 }
 
 void MySQLMgr::Disconnect()
 {
-	delete this->connection;
+    delete this->connection;
 }
 
 MySQLResult* MySQLMgr::ExecuteQueryWithData(string query)
 {
-	return this->connection->ExecuteQueryWithData(query);
+    return this->connection->ExecuteQueryWithData(query);
 }
 
 int MySQLMgr::ExecuteQueryWithOutData(string query)
@@ -40,56 +40,194 @@ int MySQLMgr::ExecuteQueryWithOutData(string query)
 
 MySQLMgr::MySQLMgr(void)
 {
-	#ifdef _WIN32
-		try
-		{
-		    this->driver = sql::mysql::get_driver_instance();
-			if (this->driver == NULL)
-				throw sql::SQLException();
-		}
-		catch (sql::SQLException e)
-		{
-			wcout << "Error creating MySQL Driver: " << e.getSQLStateCStr() << " " << e.getErrorCode() << endl;
-		}
-	#endif
-	this->connection = new MySQLConnection();
+    #ifdef _WIN32
+        try
+        {
+            this->driver = sql::mysql::get_driver_instance();
+            if (this->driver == NULL)
+                throw sql::SQLException();
+        }
+        catch (sql::SQLException e)
+        {
+            wcout << "Error creating MySQL Driver: " << e.getSQLStateCStr() << " " << e.getErrorCode() << endl;
+        }
+    #endif
+    this->connection = new MySQLConnection();
 }
 
 MySQLMgr::~MySQLMgr(void)
 {
-	delete this->connection;
+    delete this->connection;
 }
 
 // Class MySQLConnection
 MySQLResult* MySQLConnection::ExecuteQueryWithData(string query)
 {
-	MySQLResult* result = new MySQLResult();
-	#ifdef _WIN32
-		sql::Statement* stmt = this->conn->createStatement();
-		result->result = stmt->executeQuery(query);
-		delete stmt;
-	#else
+    MySQLResult* result = new MySQLResult();
+    #ifdef _WIN32
+        sql::Statement* stmt = this->conn->createStatement();
+        result->result = stmt->executeQuery(query);
+        result->row_count = result->result->rowsCount();
+        delete stmt;
+    #else
         mysql_query(this->conn, query.c_str());
-		result->result = mysql_store_result(this->conn);
-	#endif
-	return result;
+        result->result = mysql_store_result(this->conn);
+        result->row_count = mysql_num_rows(result->result);
+    #endif
+    return result;
 }
 
 int MySQLConnection::ExecuteQueryWithOutData(string query)
 {
     int num_rows_modified;
-	#ifdef _WIN32
-		sql::Statement* stmt = this->conn->createStatement();
-		num_rows_modified = stmt->executeUpdate(query);
-		delete stmt;
-	#else
+    #ifdef _WIN32
+        sql::Statement* stmt = this->conn->createStatement();
+        num_rows_modified = stmt->executeUpdate(query);
+        delete stmt;
+    #else
         mysql_query(this->conn, query.c_str());
-		num_rows_modified = mysql_affected_rows(this->conn);
-	#endif
-	return num_rows_modified;
+        num_rows_modified = mysql_affected_rows(this->conn);
+    #endif
+    return num_rows_modified;
 }
 
 MySQLConnection::~MySQLConnection(void)
 {
-	delete this->conn;
+    delete this->conn;
+}
+
+//Class MySQLResult
+void MySQLResult::fetch_row()
+{
+    #ifdef _WIN32
+        this->result->next();
+    #else
+        this->current_row = mysql_fetch_row(this->result);
+    #endif
+}
+
+int MySQLResult::get_int_field(string name)
+{
+    #ifdef _WIN32
+        return this->result->getInt(name);
+    #else
+        MYSQL_FIELD *field;
+        unsigned int num_fields = mysql_num_fields(this->result);
+        int field_index = -1;
+        for(unsigned int i = 0; (field = mysql_fetch_field(result)); i++)
+        {
+            if (strcmp(name, field->name) == 0)
+            {
+                field_index = i;
+                break;
+            }
+        }
+        if (field_index < 0)
+            return 0;
+        else
+            return atoi(this->current_row[field_index]);
+    #endif
+}
+
+int MySQLResult::get_int_field(int index)
+{
+    #ifdef _WIN32
+        return this->result->getInt(index);
+    #else
+        if (index < 0)
+            return 0;
+        else
+            return atoi(this->current_row[index]);
+    #endif
+}
+
+string MySQLResult::get_string_field(string name)
+{
+    #ifdef _WIN32
+        return this->result->getString(name);
+    #else
+        MYSQL_FIELD *field;
+        unsigned int num_fields = mysql_num_fields(this->result);
+        int field_index = -1;
+        for(unsigned int i = 0; (field = mysql_fetch_field(result)); i++)
+        {
+            if (strcmp(name, field->name) == 0)
+            {
+                field_index = i;
+                break;
+            }
+        }
+        if (field_index < 0)
+            return 0;
+        else
+            return this->current_row[field_index]);
+    #endif
+}
+
+string MySQLResult::get_string_field(int index)
+{
+    #ifdef _WIN32
+        return this->result->getString(index);
+    #else
+        if (index < 0)
+            return 0;
+        else
+            return this->current_row[index]);
+    #endif
+}
+
+bool MySQLResult::get_bool_field(string name)
+{
+    #ifdef _WIN32
+        return this->result->getBoolean(name);
+    #else
+        MYSQL_FIELD* field;
+        unsigned int num_fields = mysql_num_fields(this->result);
+        int field_index = -1;
+        for(unsigned int i = 0; (field = mysql_fetch_field(result)); i++)
+        {
+            if (strcmp(name, field->name) == 0)
+            {
+                field_index = i;
+                break;
+            }
+        }
+        if (field_index < 0)
+            return false;
+        else
+        {
+            string field_value = this->current_row[field_index];
+            if ((field_value == "TRUE") || (field_value == "true") || (field_value == "True"))
+                return true;
+            else
+                return false;
+        }
+    #endif
+}
+
+bool MySQLResult::get_bool_field(int index)
+{
+    #ifdef _WIN32
+        return this->result->getBoolean(index);
+    #else
+        if (field_index < 0)
+            return false;
+        else
+        {
+            string field_value = this->current_row[index];
+            if ((field_value == "TRUE") || (field_value == "true") || (field_value == "True"))
+                return true;
+            else
+                return false;
+        }
+    #endif
+}
+
+MySQLResult::~MySQLResult(void)
+{
+    #ifdef _WIN32
+        delete this->result;
+    #else
+        mysql_free_result(this->result);
+    #endif
 }
--- Server/mysqlmgr.h
+++ Server/mysqlmgr.h
@@ -3,15 +3,13 @@
 #include <iostream>
 
 #ifdef _WIN32
-	#include <mysql_driver.h>
-	#include <mysql_connection.h>
-	#include <cppconn/exception.h>
-	#include <cppconn/resultset.h>
-	#include <cppconn/statement.h>
-	//#pragma comment(lib, "mysqlcppconn.lib")
-	//#pragma comment(lib, "libmysql.lib")
+    #include <mysql_driver.h>
+    #include <mysql_connection.h>
+    #include <cppconn/exception.h>
+    #include <cppconn/resultset.h>
+    #include <cppconn/statement.h>
 #else
-	#include "mysql/mysql.h"
+    #include "mysql/mysql.h"
 #endif
 
 using namespace std;
@@ -19,38 +17,48 @@ using namespace std;
 class MySQLResult
 {
 public:
-	#ifdef _WIN32
-		sql::ResultSet* result;
-	#else
-		MYSQL_RES* result;
-	#endif
+    #ifdef _WIN32
+        sql::ResultSet* result;
+    #else
+        MYSQL_RES* result;
+        MYSQL_ROW current_row;
+    #endif
+    int row_count;
+    void fetch_row();
+    int get_int_field(string name);
+    int get_int_field(int index);
+    string get_string_field(string name);
+    string get_string_field(int index);
+    bool get_bool_field(string name);
+    bool get_bool_field(int index);
+    ~MySQLResult(void);
 };
 
 class MySQLConnection
 {
 public:
-	#ifdef _WIN32
-		sql::Connection* conn;
-	#else
-		MYSQL* conn;
-	#endif
-	MySQLResult* ExecuteQueryWithData(string query);
+    #ifdef _WIN32
+        sql::Connection* conn;
+    #else
+        MYSQL* conn;
+    #endif
+    MySQLResult* ExecuteQueryWithData(string query);
     int ExecuteQueryWithOutData(string query);
-	~MySQLConnection(void);
+    ~MySQLConnection(void);
 };
 
 class MySQLMgr
 {
 private:
-	MySQLConnection* connection;
+    MySQLConnection* connection;
 public:
-	#ifdef _WIN32
-		sql::mysql::MySQL_Driver *driver;
-	#endif
-	MySQLConnection* Connect (string host, string username, string password, string database);
-	void Disconnect();
-	MySQLResult* ExecuteQueryWithData(string query);
+    #ifdef _WIN32
+        sql::mysql::MySQL_Driver *driver;
+    #endif
+    MySQLConnection* Connect (string host, string username, string password, string database);
+    void Disconnect();
+    MySQLResult* ExecuteQueryWithData(string query);
     int ExecuteQueryWithOutData(string query);
-	MySQLMgr(void);
-	~MySQLMgr(void);
+    MySQLMgr(void);
+    ~MySQLMgr(void);
 };
