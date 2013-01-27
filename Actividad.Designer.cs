6dae4db155de09f3297001030ba3517f53f61206
tanituqui <tanituqui@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2013-01-27 12:52:54 +0000

    Add forgotten files
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@358 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

---
+++ Actividad.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Juego_Hotel
+{
+    partial class Actividad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mensajes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mensajes
+            // 
+            this.mensajes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mensajes.Location = new System.Drawing.Point(12, 12);
+            this.mensajes.Multiline = true;
+            this.mensajes.Name = "mensajes";
+            this.mensajes.Size = new System.Drawing.Size(779, 78);
+            this.mensajes.TabIndex = 0;
+            // 
+            // Actividad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 102);
+            this.Controls.Add(this.mensajes);
+            this.Name = "Actividad";
+            this.Text = "Actividad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mensajes;
+
+
+    }
+}
---
+++ Actividad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Juego_Hotel.Resources;
+
+namespace Juego_Hotel
+{
+    public partial class Actividad : Form
+    {
+        public Actividad()
+        {
+            InitializeComponent();
+        }
+
+        public void DadoTirado(Jugador jugador, int resultado)
+        {
+            this.mensajes.AppendText(String.Format(Mensajes.MADadoTirado, jugador.nombre_online, jugador.color.ToString(), resultado.ToString()) + Environment.NewLine);
+        }
+
+        public void PasarTurno(String jugador, String color)
+        {
+            this.mensajes.AppendText(String.Format(Mensajes.MAPasarTurno, jugador, color) + Environment.NewLine);
+        }
+
+        public void ComprarHotel(String jugador, String color, String hotel)
+        {
+            this.mensajes.AppendText(String.Format(Mensajes.MAComprarHotel, jugador, color, hotel) + Environment.NewLine);
+        }
+
+        public void Tirar_Dado_Construccion(Jugador jugador, Tipos.Resultado_dado_cons resultado)
+        {
+            this.mensajes.AppendText(String.Format(Mensajes.MATirarDadoConstruccion, jugador.nombre_online, jugador.color, resultado.ToString()) + Environment.NewLine);
+        }
+
+        public void Pedir_Noches_Online(Jugador jugador, Jugador jugadorD, int cantidad)
+        {
+            this.mensajes.AppendText(String.Format(Mensajes.MAPedirNochesOnline, jugador.nombre_online, jugador.color, jugadorD.nombre_online, jugadorD.color, cantidad) + Environment.NewLine);
+        }
+    }
+}
