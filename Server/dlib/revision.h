39e92cbb1cfd11d6faf399dacd785476bf1f8193
ifilgud <ifilgud@cbd89f15-8f70-4dcd-96d5-802d9ad3bda2>
2014-11-02 00:30:50 +0000

    Remove old DLIB and update project for VS2013. Next commmit will have the new DLIB
    git-svn-id: https://svn.code.sf.net/p/hotels-game/code@494 cbd89f15-8f70-4dcd-96d5-802d9ad3bda2

--- Server/dlib/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef DLIB_REVISION_H
-// Version:  17.44
-// Date:     Mon Nov 21 21:08:51 EST 2011
-// Mercurial Revision ID:  9ed76e89b664
-#define DLIB_MAJOR_VERSION  17
-#define DLIB_MINOR_VERSION  44
-#endif
